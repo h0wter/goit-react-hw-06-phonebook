89c30721dea7d460371666051798f295d72da9ed
h0wter <howter99@gmail.com>
2022-12-01 20:33:27 +0200

    add persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
+
+const persistConfig = {
+  key = ''
+}
 
 export const store = configureStore({
   reducer: {
